b38c3ddc22b360b9173232608f1a299c82a5705e
Susleen <114741775+Kamoe7@users.noreply.github.com>
2025-11-08 15:54:39 +0545

    footer added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,34 @@
-
 import Login from './pages/Login'
 import './App.css'
 import Navbar from './components/Navbar'
 import LoginBox from './components/LoginBox'
 import Footer from './components/Footer'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Terms from './pages/Terms.jsx'
 
 function App() {
-
-
   return (
-    <div>
-       <div className='background'>
-        <div className='bg-img'></div>
+    <Router>
+      <div>
+        <div className='background'>
+          <div className='bg-img'></div>
+        </div>
 
-          </div>
-
-     <Navbar/>
-    <LoginBox/>
-    
-    <Footer/>
-
-    </div>
-   
-  
-
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={
+            <>
+              <LoginBox />
+              <Footer />
+            </>
+          } />
+          <Route path="/terms" element={<Terms />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   )
-  
 }
 
-export default App
+export default App;
--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import "../css/footer.css"
 
 const Footer = () => {
   return (
-    <div></div>
+    <footer className="footer">
+      <div className="footer-div">
+        <div className="footer--text-section">
+          <div className="footer-brand">
+            <span className="footer--lettafaktura-text">123 Fakturera</span>
+          </div>
+          <nav className="footer-menu">
+            <a href="#">Hem</a>
+            <a href="#">Beställ</a>
+            <a href="#">Kontakta oss</a>
+          </nav>
+        </div>
+        <hr className='custom-line'></hr>
+        <div className="footer-copyright">
+          <p className="copyright-text">
+            © 2024 123 Fakturera. Alla rättigheter förbehållna.
+          </p>
+        </div>
+      </div>
+    
+    </footer>
+
   )
 }
 
